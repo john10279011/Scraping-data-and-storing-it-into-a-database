24930cfc377867c6005bbe4bf7f73c74f3f58fbd
john <johnmugure659@gmail.com>
2023-02-14 12:10:45 +0300

    initial commit

--- db.py
+++ db.py
@@ -1,23 +1,3 @@
-# import sqlite3
-
-
-# conn = sqlite3.connect("lunch.db")
-# c = conn.cursor()
-
-# # c.execute("""CREATE TABLE meals(sandwich TEXT, fruit TEXT, tablenumber INT)""")
-
-
-# sandwich = "cheese"
-# fruit = "banana"
-# tablenumber = 21
-
-# # c.execute("""INSERT INTO meals VALUES(?,?,?)""", (sandwich, fruit, tablenumber))
-# # conn.commit()
-
-# c.execute("""SELECT fruit FROM meals""")
-# results = c.fetchall()
-# print(results)
-
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import sqlite3
@@ -40,3 +20,26 @@ for item in items:
     name = item.find("h4").find("a").text
     url = "https://www.jessops.com" + str(item.find("h4").find("a")["href"])
     price = item.find("p", {"class": "price larger"}).text.strip().replace("£", "")
+    c.execute("""INSERT INTO drones VALUES(?,?,?)""", (name, url, price))
+    c.commit()
+
+
+# import sqlite3
+
+
+# conn = sqlite3.connect("lunch.db")
+# c = conn.cursor()
+
+# # c.execute("""CREATE TABLE meals(sandwich TEXT, fruit TEXT, tablenumber INT)""")
+
+
+# sandwich = "cheese"
+# fruit = "banana"
+# tablenumber = 21
+
+# c.execute("""INSERT INTO meals VALUES(?,?,?)""", (sandwich, fruit, tablenumber))
+# conn.commit()
+
+# c.execute("""SELECT fruit FROM meals""")
+# results = c.fetchall()
+# print(results)
