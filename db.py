542ae63185974f8768cfef8ecfc9a84f401b4c87
john <johnmugure659@gmail.com>
2023-02-14 11:58:11 +0300

    initial commit

--- db.py
+++ db.py
@@ -1,19 +1,35 @@
-import sqlite3
+# import sqlite3
 
 
-conn = sqlite3.connect("lunch.db")
-c = conn.cursor()
+# conn = sqlite3.connect("lunch.db")
+# c = conn.cursor()
 
-# c.execute("""CREATE TABLE meals(sandwich TEXT, fruit TEXT, tablenumber INT)""")
+# # c.execute("""CREATE TABLE meals(sandwich TEXT, fruit TEXT, tablenumber INT)""")
 
 
-sandwich = "cheese"
-fruit = "banana"
-tablenumber = 21
+# sandwich = "cheese"
+# fruit = "banana"
+# tablenumber = 21
 
-# c.execute("""INSERT INTO meals VALUES(?,?,?)""", (sandwich, fruit, tablenumber))
-# conn.commit()
+# # c.execute("""INSERT INTO meals VALUES(?,?,?)""", (sandwich, fruit, tablenumber))
+# # conn.commit()
 
-c.execute("""SELECT fruit FROM meals""")
-results = c.fetchall()
-print(results)
+# c.execute("""SELECT fruit FROM meals""")
+# results = c.fetchall()
+# print(results)
+
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+url = "https://www.jessops.com/drones?fh_start_index=21&fh_view_size=21"
+r = HTMLSession()
+
+page = r.get(url)
+soup = BeautifulSoup(page.content, "html.parser")
+
+items = soup.find_all("div", {"class": "f-grid prod-row"})
+for item in items:
+    name = item.find("h4").find("a").text
+    url = "https://www.jessops.com" + str(item.find("h4").find("a")["href"])
+    price = item.find("p", {"class": "price larger"}).text.strip().replace("£", "")
+    print(price)
