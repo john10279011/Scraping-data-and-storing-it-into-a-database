7506e1e35f75d466bc34316f6b22b474f34056c6
john <johnmugure659@gmail.com>
2023-02-14 11:27:02 +0300

    first commit

--- db.py
+++ db.py
@@ -1,1 +1,19 @@
 import sqlite3
+
+
+conn = sqlite3.connect("lunch.db")
+c = conn.cursor()
+
+# c.execute("""CREATE TABLE meals(sandwich TEXT, fruit TEXT, tablenumber INT)""")
+
+
+sandwich = "cheese"
+fruit = "banana"
+tablenumber = 21
+
+# c.execute("""INSERT INTO meals VALUES(?,?,?)""", (sandwich, fruit, tablenumber))
+# conn.commit()
+
+c.execute("""SELECT fruit FROM meals""")
+results = c.fetchall()
+print(results)
